9241ca726094cd78f803af810046a15d65387bb7
samrekik <rekiksam@gmail.com>
2025-03-23 10:41:27 +0100

    create and run server

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignOut";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./assets/components/Header";
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-out" element={<SignOut />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
